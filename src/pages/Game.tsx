b44761252de8c8f44f7bf050abda9753cc15f755
Almighty Sasquatch <almightysasquatch@gmail.com>
2022-06-26 21:05:35 +0300

    Added motion

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -6,6 +6,7 @@ import { useGameEngine } from "../context/GameEngineContext"
 import ChoiceButton from "../components/ChoiceButton"
 import PlayerCard from "../components/PlayerCard"
 import randomMessage from "../utilities/randomMessage"
+import { motion } from "framer-motion"
 
 export function Game() {
   const {
@@ -22,99 +23,126 @@ export function Game() {
 
   return (
     <>
-      <Row className="mt-5">
-        <Col className="d-flex justify-content-center">
-          <h1 className="text-white">Choose your weapon</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="d-flex justify-content-center gap-3 mt-3">
-          {gameChoices.map((choice) => (
-            <ChoiceButton
-              key={choice.id}
-              id={choice.id}
-              name={choice.name}
-              img={choice.img}
-            />
-          ))}
-        </Col>
-      </Row>
-      {playerChoice && (
-        <Row xs={3} className="g-3 mt-2 align-items-start">
-          <Col className="d-flex justify-content-center flex-column align-items-center">
-            <PlayerCard
-              img="/imgs/gamer.png"
-              name="Pro Player"
-              pick={playerChoice.name}
-              score={playerHealth}
-            />
-            <ChoiceCard {...playerChoice} />
+      <motion.div
+        animate={{ y: [-1000, 0] }}
+        transition={{ ease: "easeOut", duration: 0.6 }}
+      >
+        <Row className="mt-5">
+          <Col className="d-flex justify-content-center">
+            <h1 className="text-white">Choose your weapon</h1>
           </Col>
-          <Col className="d-flex justify-content-center align-items-center flex-column gap-3">
-            <div>
-              <img src="/imgs/vs.png" className="img-fluid" width={70}></img>
-            </div>
-            {gameResult && (
-              <div>
-                {gameResult === "You win!" ? (
-                  <img
-                    src="/imgs/win.png"
-                    className="img-fluid"
-                    width={200}
-                  ></img>
-                ) : gameResult === "You lose!" ? (
-                  <img
-                    src="/imgs/lose.png"
-                    className="img-fluid"
-                    width={200}
-                  ></img>
-                ) : (
-                  <h1 className="text-white">DRAW</h1>
-                )}
-              </div>
-            )}
-          </Col>
-          {isPlaying && (
-            <Col className="d-flex justify-content-center align-items-center flex-column gap-3">
-              <span className="fs-2 text-white">{randomMessage()}</span>
-              <Spinner animation="border" variant="danger" />
-              <img
-                src="/imgs/enemy.png"
-                className="img-fluid"
-                width={200}
-              ></img>
-            </Col>
-          )}
-          {!isPlaying && computerChoice && (
-            <Col className="d-flex justify-content-center flex-column align-items-center">
-              <PlayerCard
-                img="/imgs/enemy.png"
-                name="Evil Enemy"
-                pick={computerChoice.name}
-                score={computerHealth}
-              />
-              <ChoiceCard {...computerChoice} />
-            </Col>
-          )}
         </Row>
+      </motion.div>
+      <motion.div
+        animate={{ y: [-1000, 0] }}
+        transition={{ ease: "easeOut", duration: 0.6, delay: 0.5 }}
+      >
+        <Row>
+          <Col className="d-flex justify-content-center gap-3 mt-3">
+            {gameChoices.map((choice) => (
+              <ChoiceButton
+                key={choice.id}
+                id={choice.id}
+                name={choice.name}
+                img={choice.img}
+              />
+            ))}
+          </Col>
+        </Row>
+      </motion.div>
+      {playerChoice && (
+        <>
+          <motion.div
+            animate={{ y: [1000, 0] }}
+            transition={{ ease: "easeOut", duration: 0.6 }}
+          >
+            <Row xs={3} className="g-3 mt-2 align-items-start">
+              <Col className="d-flex justify-content-center flex-column align-items-center">
+                <PlayerCard
+                  img="/imgs/gamer.png"
+                  name="Pro Player"
+                  pick={playerChoice.name}
+                  score={playerHealth}
+                />
+                <ChoiceCard {...playerChoice} />
+              </Col>
+              <Col className="d-flex justify-content-center align-items-center flex-column gap-3">
+                <div>
+                  <img
+                    src="/imgs/vs.png"
+                    className="img-fluid"
+                    width={70}
+                  ></img>
+                </div>
+                {gameResult && (
+                  <div>
+                    {gameResult === "You win!" ? (
+                      <img
+                        src="/imgs/win.png"
+                        className="img-fluid"
+                        width={200}
+                      ></img>
+                    ) : gameResult === "You lose!" ? (
+                      <img
+                        src="/imgs/lose.png"
+                        className="img-fluid"
+                        width={200}
+                      ></img>
+                    ) : (
+                      <h1 className="text-white">DRAW</h1>
+                    )}
+                  </div>
+                )}
+              </Col>
+              {isPlaying && (
+                <Col className="d-flex justify-content-center align-items-center flex-column gap-3">
+                  <span className="fs-2 text-white">{randomMessage()}</span>
+                  <Spinner animation="border" variant="danger" />
+                  <img
+                    src="/imgs/enemy.png"
+                    className="img-fluid"
+                    width={200}
+                  ></img>
+                </Col>
+              )}
+              {!isPlaying && computerChoice && (
+                <Col className="d-flex justify-content-center flex-column align-items-center">
+                  <PlayerCard
+                    img="/imgs/enemy.png"
+                    name="Evil Enemy"
+                    pick={computerChoice.name}
+                    score={computerHealth}
+                  />
+                  <ChoiceCard {...computerChoice} />
+                </Col>
+              )}
+            </Row>
+          </motion.div>
+        </>
       )}
-      <Row className="mt-5">
-        <Col className="d-flex align-items-center flex-column">
-          {playerChoice && (
-            <Button
-              onClick={() => resetGame()}
-              variant="outline-danger"
-              className="mt-3 mb-3"
-            >
-              {playerHealth <= 0 || computerHealth <= 0
-                ? "Play again"
-                : "Restart"}
-            </Button>
-          )}
-          <h3 className="text-white">Your victories: {highScore}</h3>
-          <h3 className="text-white">Deaths: {deaths}</h3>
-        </Col>
-      </Row>
+      <motion.div
+        animate={{ y: [-1000, 0] }}
+        transition={{ ease: "easeOut", duration: 0.6, delay: 1 }}
+      >
+        <Row className="mt-5">
+          <Col className="d-flex align-items-center flex-column">
+            {playerChoice && (
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                onClick={() => resetGame()}
+                className="btn btn-outline-danger btn-lg mb-2"
+              >
+                {playerHealth <= 0 || computerHealth <= 0
+                  ? "Play again"
+                  : "Restart"}
+              </motion.button>
+            )}
+            <h3 className="text-white">Your victories: {highScore}</h3>
+            <h3 className="text-white">Deaths: {deaths}</h3>
+          </Col>
+        </Row>
+      </motion.div>
     </>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,39 +1,50 @@
 import { Button, Col, Container, Row } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
+import { motion } from "framer-motion"
 
 export function Home() {
+  console.log(motion)
   return (
     <>
-      <Row className="mt-5">
-        <Col className="d-flex justify-content-center">
-          <h1 className="text-white">Rock, Paper, Scissors, Lizard, Spock</h1>
-        </Col>
-      </Row>
-      <Row
-        className="mt-5 rounded p-5"
-        style={{
-          backgroundColor: "rgba(245, 245, 245, 0.75)",
-          boxShadow: "0px 0px 10px #e3e3e3",
-        }}
+      <motion.div
+        animate={{ y: [-300, 0] }}
+        transition={{ ease: "easeOut", duration: 0.6 }}
       >
-        <Col className="d-flex align-items-center flex-column">
-          <p className="text-black fs-3 text-center">
-            Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard, Lizard
-            poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard,
-            Lizard eats Paper, Paper disproves Spock, Spock vaporizes Rock, (and
-            as it always has) Rock crushes Scissors
-          </p>
-          <p className="text-black fs-3 text-center">
-            Made with love, by Elvis
-          </p>
+        <Row className="mt-5">
+          <Col className="d-flex justify-content-center">
+            <h1 className="text-white">Rock, Paper, Scissors, Lizard, Spock</h1>
+          </Col>
+        </Row>
+        <Row
+          className="mt-5 rounded p-5"
+          style={{
+            backgroundColor: "rgba(245, 245, 245, 0.75)",
+            boxShadow: "0px 0px 10px #e3e3e3",
+          }}
+        >
+          <Col className="d-flex align-items-center flex-column">
+            <p className="text-black fs-3 text-center">
+              Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard,
+              Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates
+              Lizard, Lizard eats Paper, Paper disproves Spock, Spock vaporizes
+              Rock, (and as it always has) Rock crushes Scissors
+            </p>
+            <p className="text-black fs-3 text-center">
+              Made with love, by Elvis
+            </p>
 
-          <NavLink to="/game">
-            <Button variant="danger" size="lg">
-              Play
-            </Button>
-          </NavLink>
-        </Col>
-      </Row>
+            <NavLink to="/game">
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                className="btn btn-danger btn-lg mt-5"
+              >
+                Play
+              </motion.button>
+            </NavLink>
+          </Col>
+        </Row>
+      </motion.div>
     </>
   )
 }
